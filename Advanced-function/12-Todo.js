f592ff82387d7b274f31e85e92cf305101d88c4c
shahfaisal3232222 <sfaisalshah@gmail.com>
2025-06-24 16:43:28 +0500

    add new files

---
+++ Advanced-function/12-Todo.js
@@ -0,0 +1,68 @@
+const todoList = [{name: 'make dinner',
+      dueDate: '22-25.11',
+   },
+   {
+      name:'wash clothes',
+      dueDate:'23-2-22'
+   }];
+
+
+  renderTodoList();
+   function renderTodoList(){
+       let todoListHTML = '';
+   
+      todoList.forEach(function(todoObject,index){
+          
+      const {name,dueDate} = todoObject; //this is called destructuring when we get the values in the object
+      const html = `
+      <div>${name}</div>
+      <div> ${dueDate}</div>
+       <button class="delete-button js-todo-delete-button">Delete</button> 
+       `;
+      todoListHTML += html
+      });
+
+      document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+
+      document.querySelectorAll(".js-todo-delete-button")
+      .forEach((deleteButton , index) => {
+         deleteButton.addEventListener("click" , () => {
+            todoList.splice(index , 1)
+               renderTodoList();
+         })
+      });
+  };
+      document.querySelector(".js-todo-button")
+         .addEventListener("click" , () => {
+            addTodo();
+         });
+   
+   
+   function addTodo(){
+      const inputElement = document.querySelector('.js-name-input');
+      let name = inputElement.value;
+
+      const dateinputElement = document.querySelector('.js-due-date-input');
+      const dueDate = dateinputElement.value;
+      
+
+
+      if(!name){
+         alert("please enter somethings")
+         return;
+      }
+      if(!dueDate){
+         alert("please enter due date")
+         return;
+      }
+      
+      todoList.push({name , dueDate});
+      inputElement.value = '';
+      dateinputElement.value = '';
+      renderTodoList();
+
+
+   }
+
+   
